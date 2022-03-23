56d6f75772b607d906a7ec144b37334d25b92675
frand-kod <85376495+frand-kod@users.noreply.github.com>
2022-03-24 01:15:06 +0700

    Add files via upload

---
+++ Class_dan_Object_taxi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object_taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set ObjProp
+            Class_taxi taxi = new Class_taxi();
+
+            //set ValueProp
+            taxi.DrivName = "Frand Odi Anggoro";
+            taxi.OnDuty = true;
+            taxi.NumPassengger = 10;
+
+            //calling method
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassengger();
+            taxi.DropOffPassengger();
+
+            Console.ReadKey();
+        }
+    }
+}
